e18a0922d9dd3b40d9d0cbca993aa91fcce2dc69
kinncj <kinncj@php.net>
2014-12-10 14:57:03 -0500

    Added symfony2 as example application

--- demoscript.sh
+++ demoscript.sh
@@ -5,6 +5,6 @@ wget https://getcomposer.org/download/1.0.0-alpha9/composer.phar
 
 chmod +x composer.phar
 
-php composer.phar create-project symfony/framework-standard-edition /var/www/symfony2 '~2.7'
+php composer.phar create-project symfony/framework-standard-edition /var/www/symfony2 '~2.6'
 
 chmod -R 777 /var/www/symfony2/app/{logs,cache}
