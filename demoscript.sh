af292ab4d1790ae510f72db5920d38bd30030bdf
kinncj <kinncj@php.net>
2014-12-10 16:13:27 -0500

    updated demoscript

--- demoscript.sh
+++ demoscript.sh
@@ -10,24 +10,16 @@ php composer.phar create-project symfony/framework-standard-edition /var/www/sym
 chmod -R 777 /var/www/symfony2/app/{logs,cache}
 
 
-cat <<EOT >/var/www/symfony2/web/index.php
-<?php
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-Debug::enable();
-require_once __DIR__.'/../app/AppKernel.php';
-$kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
-
-EOT
+echo '<?php' >> /var/www/symfony2/web/app_dev.php
+echo 'use Symfony\Component\HttpFoundation\Request;' >> /var/www/symfony2/web/app_dev.php
+echo 'use Symfony\Component\Debug\Debug;' >> /var/www/symfony2/web/app_dev.php
+echo '$loader = require_once __DIR__."/../app/bootstrap.php.cache";' >> /var/www/symfony2/web/app_dev.php
+echo 'Debug::enable();' >> /var/www/symfony2/web/app_dev.php
+echo 'require_once __DIR__."/../app/AppKernel.php";' >> /var/www/symfony2/web/app_dev.php
+echo '$kernel = new AppKernel("dev", true);' >> /var/www/symfony2/web/app_dev.php
+echo '$kernel->loadClassCache();' >> /var/www/symfony2/web/app_dev.php
+echo '$request = Request::createFromGlobals();' >> /var/www/symfony2/web/app_dev.php
+echo '$response = $kernel->handle($request);' >> /var/www/symfony2/web/app_dev.php
+echo '$response->send();' >> /var/www/symfony2/web/app_dev.php
+echo '$kernel->terminate($request, $response);' >> /var/www/symfony2/web/app_dev.php
+echo '<?php header("location: app_dev.php");'> /var/www/symfony2/web/index.php
